152bd409204b9ae9cdbf9912ffeacfcc5ce75754
sonti123l <116090293+sonti123l@users.noreply.github.com>
2024-07-08 06:22:37 +0530

    Delete new_decorator.py

--- new_decorator.py
+++
@@ -1,7 +0,0 @@
-def no_of_times(func):
-    def Wrapper_function(*args,**kwargs):
-        value = func(*args,**kwargs)
-        for i in range(value):
-            print("Hello World")
-        return value
-    return Wrapper_function  
